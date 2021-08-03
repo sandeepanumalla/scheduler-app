0dcff0a8db69a6a60ae69334a37fe876632d0f43
= <anumalla.sandeep1999@gmail.com>
2021-08-03 16:18:40 +0530

    weekly function working

--- client/src/components/calendar/Weekly.jsx
+++ client/src/components/calendar/Weekly.jsx
@@ -9,127 +9,155 @@ const usePrevious = (value) => {
 };
 
 const Weekly = ({ takePrev, takeNext }) => {
-  const [currentDate, setcurrentDate] = useState(new Date().getDate());
-  const [currentYear, setcurrentYear] = useState(new Date().getFullYear());
-  const [currentDateDay, setCurrentDateDay] = useState(new Date().getDay());
-  const [firstDayOfLastWeek, setfirstDayOfLastWeek] = useState();
-  const [firstDayOfNextWeek, setfirstDayOfNextWeek] = useState();
-  const [currentMonth, setcurrentMonth] = useState(new Date().getMonth());
-  const arr = [];
+  let [state, setstate] = useState({
+    arr: [],
+    obj: {
+      firstDay: "",
+      lastDay: "",
+      monthOfFirstDay: "",
+      currentMonth: new Date().getMonth() + 1,
+      currentYear: new Date().getFullYear(),
+      currentDay: new Date().getDate(),
+      day: new Date().getDay(),
+    },
+  });
+  let { arr, obj } = state;
+
+  useEffect(() => {
+    let lastDay = obj.firstDay + 7;
+
+    setstate((obj.lastDay = lastDay));
+  }, []);
 
   const previous = usePrevious(takePrev);
   const next = usePrevious(takeNext);
 
-  function arrangeWeek(arr){
-    let increment = currentDate;
-      let lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
-     // console.log("29", lastDayOfMonth);
-      if (currentDateDay > 0) {
-        let decrement = currentDate - 1;
-        for (let i = currentDateDay - 1; i >= 0; i--) {
-          arr[i] = decrement;
-          decrement--;
-        }
-      }
-      for (let i = currentDateDay; i < 7; i++) {
-      //  console.log("last day" + lastDayOfMonth);
-        console.log("current month" + currentMonth);
-        if (lastDayOfMonth === increment) {
-          if (currentMonth === 12) {
-            let newYear = currentYear + 1;
-            setcurrentYear(newYear);
-            setcurrentMonth(1);
-          } else {
-            arr[i] = lastDayOfMonth;
-            let newMonth = currentMonth + 1;
-            setcurrentMonth(newMonth);
-          }
-        }
-        arr[i] = increment;
-        increment++;
-      }
-      let lastElement = arr[arr.length - 1];
-      if (currentMonth === 1) {
-        let year = currentYear - 1;
-        let lastDate = new Date(year, 12, 0);
-        if (lastDate === lastElement) {
-          setfirstDayOfNextWeek(1);
+  function arrangeWeek(arr, obj, operation) {
+    let newObj;
+    if (operation === "next") {
+      let { currentMonth, firstDay, lastDay, currentYear } = obj;
+      let lastDayOfCurrentMonth = new Date(
+        currentYear,
+        currentMonth,
+        0
+      ).getDate();
+      console.log("last day of current month", lastDayOfCurrentMonth);
+      if (lastDay === lastDayOfCurrentMonth) {
+        if (currentMonth === 12) {
+          currentYear = currentYear + 1;
+          currentMonth = 1;
+          lastDay = 1;
         } else {
-          setfirstDayOfNextWeek(lastElement);
-        }
-      } else {
-        let lastDateOfMonth = new Date(currentYear,currentMonth, 0);
-        console.log(lastDateOfMonth);
-        if (lastElement === lastDateOfMonth) {
-          setfirstDayOfNextWeek(lastElement);
-        } else {
-          setfirstDayOfNextWeek(1);
+          currentMonth = currentMonth + 1;
+          lastDay = 1;
         }
 
-        setfirstDayOfLastWeek(arr[0]);
+        // also check the month that if it is december or not to increment the new year
+      } else {
+        lastDay++;
+
+        for (let i = 0; i < 7; i++) {
+          if (lastDay > lastDayOfCurrentMonth) {
+            if (currentMonth === 12) {
+              currentYear = currentYear + 1;
+              currentMonth = 1;
+              lastDay = 1;
+            } else {
+              currentMonth = currentMonth + 1;
+              lastDay = 1;
+            }
+
+            // also check the month that if it is december or not to increment the new year
+          }
+          arr.push(lastDay++);
+        }
+        return {
+          arr,
+          newObj: {
+            firstDay: arr[0],
+            lastDay: arr[arr.length - 1],
+            currentMonth: currentMonth,
+            currentYear: currentYear,
+          },
+        };
       }
-      return arr;
+    } else if (operation === "previous") {
+      let { firstDay, lastDay, currentMonth, currentYear } = obj;
+
+      let currentDay = lastDay - 1;
+
+      for (let i = 6; i >= 0; i--) {
+        if (currentDay < 0) {
+          if (currentMonth === 1) {
+            currentMonth = 12;
+            currentYear = currentYear - 1;
+            currentDay = new Date(currentYear, currentMonth, 0).getDate();
+          } else {
+            currentDay = new Date(currentYear, currentMonth, 0).getDate();
+          }
+        }
+        arr[i] = currentDay--;
+      }
+      return {
+        arr,
+        newObj: {
+          firstDay: arr[0],
+          lastDay: arr[arr.length - 1],
+          currentMonth,
+          currentYear,
+        },
+      };
+    } else {
+      let { day, currentDay, firstDay, lastDay, currentMonth, currentYear } =
+        obj;
+      let newCurrentDay = currentDay - 1;
+
+      for (let i = day; i < 7; i++) {
+        arr[i] = currentDay++;
+      }
+      //currentDay = currentDay - 1;
+      for (let i = day - 1; i >= 0; i--) {
+        if (newCurrentDay < 1) {
+          if (currentMonth === 1) {
+            currentMonth = 12;
+            currentYear = currentYear - 1;
+          } else {
+            currentMonth = currentMonth - 1;
+            newCurrentDay = new Date(currentYear, currentMonth, 0).getDate();
+          }
+        }
+        arr[i] = newCurrentDay;
+        newCurrentDay--;
+      }
+      return {
+        arr,
+        obj: {
+          firstDay: arr[0],
+
+          lastDay: arr[arr.length - 1],
+          currentYear,
+          currentMonth,
+        },
+      };
+    }
   }
 
   useEffect(() => {
     if (previous !== takePrev) {
     } else if (next !== takeNext) {
     } else {
-    //   let increment = currentDate;
-    //   let lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
-    //  // console.log("29", lastDayOfMonth);
-    //   if (currentDateDay > 0) {
-    //     let decrement = currentDate - 1;
-    //     for (let i = currentDateDay - 1; i >= 0; i--) {
-    //       arr[i] = decrement;
-    //       decrement--;
-    //     }
-    //   }
-    //   for (let i = currentDateDay; i < 7; i++) {
-    //   //  console.log("last day" + lastDayOfMonth);
-    //     console.log("current month" + currentMonth);
-    //     if (lastDayOfMonth === increment) {
-    //       if (currentMonth === 12) {
-    //         let newYear = currentYear + 1;
-    //         setcurrentYear(newYear);
-    //         setcurrentMonth(1);
-    //       } else {
-    //         arr[i] = lastDayOfMonth;
-    //         let newMonth = currentMonth + 1;
-    //         setcurrentMonth(newMonth);
-    //       }
-    //     }
-    //     arr[i] = increment;
-    //     increment++;
-    //   }
-    //   let lastElement = arr[arr.length - 1];
-    //   if (currentMonth === 1) {
-    //     let year = currentYear - 1;
-    //     let lastDate = new Date(year, 12, 0);
-    //     if (lastDate === lastElement) {
-    //       setfirstDayOfNextWeek(1);
-    //     } else {
-    //       setfirstDayOfNextWeek(lastElement);
-    //     }
-    //   } else {
-    //     let lastDateOfMonth = new Date(currentYear,currentMonth, 0);
-    //     console.log(lastDateOfMonth);
-    //     if (lastElement === lastDateOfMonth) {
-    //       setfirstDayOfNextWeek(lastElement);
-    //     } else {
-    //       setfirstDayOfNextWeek(1);
-    //     }
+      //setstate(arrangeWeek(arr, obj));
+      let { firstDay, lastDay } = obj;
 
-    //     setfirstDayOfLastWeek(arr[0]);
-    //   }
-    console.log(arrangeWeek(arr));
+      // setstate({...state,firstDay:firstDay})
+      console.log(arrangeWeek(arr, obj));
     }
   }, [takeNext, takePrev, next, previous]);
 
   console.log(arr);
   return (
     <div className="weekly_container">
-      <div>{JSON.stringify(...arr)}</div>
+      <div>This is weekly</div>
     </div>
   );
 };
--- connection/connection.js
+++ connection/connection.js
@@ -6,19 +6,34 @@ require("dotenv").config();
 //     password: "Sandeep99!",
 //     database: "new_schema"
 // })
+var db;
+function handleDisconnect() {
+  db = mysql.createConnection({
+    host: process.env.MYSQL_ADDON_HOST,
+    database: process.env.MYSQL_ADDON_DB,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+  });
 
-var db = mysql.createConnection({
-  host: process.env.MYSQL_ADDON_HOST,
-  database: process.env.MYSQL_ADDON_DB,
-  user: process.env.MYSQL_ADDON_USER,
-  password: process.env.MYSQL_ADDON_PASSWORD,
-});
+  db.connect(function (err) {
+    if (err) {
+      console.log("error when connecting to db: ", err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log("db connected successfully");
+    }
+  });
 
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("db is connected");
-});
+  db.on("error", function (err) {
+    console.log("db error", db);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = db;
